d575a033f619b47bdad3854ce6e357687ed2030c
fredmloth <frederiekeloth@gmail.com>
2025-10-30 15:41:49 +0100

    sphere and torus functions

--- main.py
+++ main.py
@@ -1,13 +1,27 @@
+import numpy as np
 # Define shape of box to sample from
 # case a box from -1 to 1 for all dimensions)
 
 # random nr generator (module or ourselves)
 
-# Function for sphere
 def sphere(x, y, z, k):
-    return 
+    #raise error for k <= 0
 
-# Function for Torus
+    # Check if the values are inside the sphere and return True
+    if x*x + y*y + z*z <= k:
+        return True
+    
+    return False
+
+
+def torus(x, y, z, bigR, smallr):
+    #raise error for bigR, smallr <= 0
+
+    # Check if values are inside Torus and return True
+    if (np.sqrt(x*x + y*y) - bigR) ** 2 + z*z <= smallr ** 2:
+        return True
+    
+    return False
 
 # Monte carlo integration
 
@@ -15,3 +29,4 @@ def sphere(x, y, z, k):
 
 # Sampling function for volume intersection with bounding box? -> check what is bounding box
 # Bounding box: space that encompases both together fully from which we sample 
+# do not sample any 3 combinations multiple times
