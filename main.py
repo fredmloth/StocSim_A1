5d3ae4c56939f26ed9fa503d27b04d74007cc423
fredmloth <frederiekeloth@gmail.com>
2025-10-30 16:17:12 +0100

    added functions

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
+# This module runs the code for assignment 1 of Stochastic Simulation
+
 import numpy as np
+import matplotlib as plt
+
 # Define shape of box to sample from
 # case a box from -1 to 1 for all dimensions)
 
@@ -23,10 +27,43 @@ def torus(x, y, z, bigR, smallr):
     
     return False
 
+
 # Monte carlo integration
+def montecarlo(radius, k, bigr, smallr):
+    
+    x, y, z = uniformrandom(radius)
+
+    if sphere(x, y, z, k) and torus(x, y, z, bigr, smallr):
+        print(f"intersection at point x:{x}, y:{y}, z:{z}")
+
+    return #volume
+
 
 # Uniform random sampling
+def uniformrandom(radius):
+    x = np.random.uniform(-radius, radius)
+    print(f"x")
+    y = np.random.uniform(-radius, radius)
+    z = np.random.uniform(-radius, radius)
+    
+    return x, y, z
 
+
+def plotintersection():
+    return 
+
+def main():
+    radius = 1.1
+    k = 1
+    bigr = 0.75
+    smallr = 0.4
+
+    # number of measurements
+    throws = 10
+    montecarlo(radius, k, bigr, smallr)
+
+main()
 # Sampling function for volume intersection with bounding box? -> check what is bounding box
 # Bounding box: space that encompases both together fully from which we sample 
 # do not sample any 3 combinations multiple times
+
