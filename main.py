a6b6fd552eb34b2dfaa1a697f04bff77327b3c4f
fredmloth <frederiekeloth@gmail.com>
2025-10-30 15:33:00 +0100

    added main

---
+++ main.py
@@ -0,0 +1,17 @@
+# Define shape of box to sample from
+# case a box from -1 to 1 for all dimensions)
+
+# random nr generator (module or ourselves)
+
+# Function for sphere
+def sphere(x, y, z, k):
+    return 
+
+# Function for Torus
+
+# Monte carlo integration
+
+# Uniform random sampling
+
+# Sampling function for volume intersection with bounding box? -> check what is bounding box
+# Bounding box: space that encompases both together fully from which we sample 
